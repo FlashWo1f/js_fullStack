b59df1f2076fc3976ca1a158e76dd5bc9cf99963
FlashWo1f <1064048947@qq.com>
2019-05-02 23:09:41 +0800

    nianzhongjian calculate

---
+++ CSS/js/calculate/index.js
@@ -0,0 +1,30 @@
+
+// 会计  计算年终奖的函数
+// 绩效 S  4
+//     A  3
+//     B  2
+//     C  1
+//     D  0
+// return perfomanceLevel == 'S' ? salary * 4 : (perfomanceLevel == 'A' ? salary * 3 : (perfomanceLevel == 'B' ? salary * 2 : (perfomanceLevel == 'A' ? salary * 1 : 0)))
+var perfomanceS = function(salary){
+    return salary * 5
+}
+var perfomanceA = function(salary){
+    return salary * 3
+}
+var perfomanceB = function(salary){
+    return salary * 2
+}
+var perfomanceC = function(salary){
+    return salary * 1
+}
+
+var calculateBounce = function (perfomanceLevel, salary) {
+    for(var i = 0, per =[]; per['X', 'S', 'A', 'B', 'C'][i++];)
+    {
+        (function(per){
+
+        })(per)
+    }
+}
+console.log(calculateBounce('A',100))
