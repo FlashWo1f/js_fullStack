9f710382fcfc27bc1a7a392bbde77f6413017a5f
FlashWo1f <1064048947@qq.com>
2019-05-30 22:19:00 +0800

    vue demo

--- vue-demo/element/src/components/Login.vue
+++ vue-demo/element/src/components/Login.vue
@@ -29,6 +29,12 @@ export default {
       if (this.user && this.password) {
         // 将数据缓存到浏览器
         localStorage.setItem('userName', this.user)
+        // 字符串
+        // this.$router.push('/main')
+        // 对象
+        // this.$router.push({ path: '/main' })
+        // 编程式跳转
+        this.$router.push({ name: 'Main', params: { userId: 1 } })
       }
     }
   }
