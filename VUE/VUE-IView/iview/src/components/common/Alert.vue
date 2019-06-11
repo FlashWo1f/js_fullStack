15a4a56026c6793c7c8c72516c3786206ee5412d
FlashWo1f <1064048947@qq.com>
2019-06-11 22:22:31 +0800

    父子组件传值

---
+++ VUE/VUE-IView/iview/src/components/common/Alert.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="alert">
+    {{msg}}
+    <br/>
+    <i-button type="success" @click="hidden">一键变帅</i-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  // properties 小程序类似
+  props: ['msg'],
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    hidden() {
+      // 向外发送
+      this.$emit('fromChild', false)
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .alert{
+    font-size: 30px;
+    text-align: center;
+    width: 300px;
+    height: 200px;
+    border: 1px solid #aaa;
+    border-radius: 5px;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 999;
+    background-color: pink;
+  }
+</style>
