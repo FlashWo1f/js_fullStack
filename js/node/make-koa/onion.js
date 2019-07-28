011ad6d03eb9920c4c411053e3b1e21896cfe7f9
FlashWo1f <1064048947@qq.com>
2019-07-28 23:47:41 +0800

    koa

---
+++ js/node/make-koa/onion.js
@@ -0,0 +1,14 @@
+// koa 洋葱模型
+const Koa = require('koa')
+const app = new Koa()
+app.use(async (ctx, next) => {
+  console.log(1)
+  await next()
+  console.log(2)
+})
+app.use(async (ctx) => {
+  ctx.body = '123',
+  console.log(3)
+})
+app.listen(9999)
+// 1 3 2
