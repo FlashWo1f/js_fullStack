d8a03818b071a3be2f1a2caa32d433f3fa05d861
FlashWo1f <1064048947@qq.com>
2019-08-02 23:44:58 +0800

    extend

---
+++ interview/question/extend/combineExtend.js
@@ -0,0 +1,40 @@
+function Animal(name) {
+  this.name = name
+  this.sleep = function () {
+    return this.name + '...'
+  }
+}
+
+Animal.prototype.eat = function(food) {
+  console.log(this.name + ' is eating ' + food)
+}
+// 组合继承
+
+function Cat(name) {
+  Animal.call(this)
+  this.name = name || 'tom'
+}
+let cat = new Cat()
+console.log(cat.name)
+console.log(cat.sleep())
+console.log(cat instanceof Cat)
+console.log(cat instanceof Animal)  /// false
+
+// ------------------------------
+// 寄生继承
+function Cat1(name) {
+  Animal.call(this)
+  this.name = name || 'Jim'
+}
+
+(function() {
+  // 创建一个没有实例方法的类
+  var Super = function() {}
+  Super.prototype = Animal.prototype
+  Cat1.prototype = new Super()
+})()
+let cat1 = new Cat1()
+console.log('----', cat1.name)
+console.log('----', cat1.sleep())
+console.log('----', cat1 instanceof Cat1)
+console.log('----', cat1 instanceof Animal)
