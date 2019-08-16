faf5581f6fe905c580dfdc739c1dee0a0217f1a1
FlashWo1f <1064048947@qq.com>
2019-08-16 11:18:43 +0800

    redux-source

---
+++ react/make-react-redux/src/redux/index.js
@@ -0,0 +1,11 @@
+import createStore from './createStore';
+import combineReducers from './combineReducers';
+import applyMiddleware from './applyMiddleWare';
+
+export {
+    createStore,
+    combineReducers,
+    // bindActionCreators,
+    applyMiddleware,
+    // compose,
+  }
