a350f6ff68714ca30576552b15b453717ff7b1fa
FlashWo1f <1064048947@qq.com>
2019-08-16 11:19:28 +0800

    react-router resouce

---
+++ react/make-react-router/src/react-router/Router.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import RouterContext from "./RouterContext";
+
+class Router extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: props.history.location
+        }
+        this.unlisten = props.history.listen(location => {
+            this.setState({
+                location
+            })
+        })
+    }
+
+    componentWillUnmount() {
+        if(this.unlisten) this.unlisten()
+    }
+
+    render() {
+        return (
+            <RouterContext.Provider
+                children={this.props.children}
+                value={{
+                    history: this.props.history,
+                    location: this.state.location
+                }}
+            />
+        )
+    }
+}
+export default Router;
